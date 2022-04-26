16593d42b5446adeea2dd52aca6e84ebd7be9ff4
Piccora <tatuanminh31032004@gmail.com>
2022-04-26 16:55:48 +0700

    Added instructions and questions for main file

---
+++ tarot_detail.py
@@ -0,0 +1,14 @@
+introduction = 'The tarot is a pack of playing cards, used from at least the mid-15th century in various parts of Europe to play games such as Italian tarocchini, French tarot and Austrian Königrufen, many of which are still played today. You can read more about Tarot cards on this website: "https://www.tarot.com/tarot/cards"'
+
+instructions_1 = "To start with, you will choose your questions among these fives:"
+question_1 = "[1] How is my current relationship with my partner?"
+question_2 = "[2] How will my love life in the next 6 months is going to go? (Choose this if you're single)"
+question_3 = "[3] How will my love life in the next 6 months is going to go? (Choose this if you're married)"
+question_4 = "[4] What is the current situation/problem with my studies?"
+question_5 = "[5] How will my studies progress in the next 6 months?"
+instructions_2 = "Then, think about the question that you just chose very carefully. \nShuffle the tarot deck and then fan it out in a fan shape, similar to playing poker. \nChoose three cards from the cards that you just fanned out. \nLet your heart decides the cards to choose. \nAfter choosing each card, put them separate from the pile of cards, but DO NOT change the order of the cards that you just picked. \nThis is very important for an accurate tarot reading."
+
+
+
+# print(introduction)
+print(instructions_2)
