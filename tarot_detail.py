77d3546da3e3af8d3fbfd81e6217442b480b0772
Piccora <tatuanminh31032004@gmail.com>
2022-04-27 22:27:15 +0700

    Added cards postions translated to meaning

--- tarot_detail.py
+++ tarot_detail.py
@@ -2,13 +2,109 @@ introduction = 'The tarot is a pack of playing cards, used from at least the mid
 
 instructions_1 = "To start with, you will choose your questions among these fives:"
 question_1 = "[1] How is my current relationship with my partner?"
-question_2 = "[2] How will my love life in the next 6 months is going to go? (Choose this if you're single)"
-question_3 = "[3] How will my love life in the next 6 months is going to go? (Choose this if you're married)"
-question_4 = "[4] What is the current situation/problem with my studies?"
-question_5 = "[5] How will my studies progress in the next 6 months?"
+question_2 = "[2] How will my love life in general in the next 6 months is going to go?"
+question_3 = "[3] How is my job prospect in the next few years?"
 instructions_2 = "Then, think about the question that you just chose very carefully. \nShuffle the tarot deck and then fan it out in a fan shape, similar to playing poker. \nChoose three cards from the cards that you just fanned out. \nLet your heart decides the cards to choose. \nAfter choosing each card, put them separate from the pile of cards, but DO NOT change the order of the cards that you just picked. \nThis is very important for an accurate tarot reading."
 
+major_arcana_to_symbols = """
+    The Major Arcana:
+    
+    The Fool = 0
+    The Magician = I
+    The High Priestess = II
+    The Empress = III
+    The Emperor = IV
+    The Hierophant = V
+    The Lovers = VI
+    The Chariot = VII
+    Strength = VIII
+    The Hermit = IX
+    Wheel of Fortune = X
+    Justice = XI
+    The Hanged Man = XII
+    Death = XIII
+    Temperance = XIV
+    The Devil = XV
+    The Tower = XVI
+    The Star = XVII
+    The Moon = XVIII
+    The Sun - XIX
+    Judgement = XX
+    The World = XXI
+"""
 
+wands_to_symbols = """
+    The Wands:
+    
+    Ace of Wands = 1_wands
+    Two of Wands = 2_wands
+    Three of Wands = 3_wands
+    Four of Wands = 4_wands
+    Five of Wands = 5_wands
+    Six of Wands = 6_wands
+    Seven of Wands = 7_wands
+    Eight of Wands = 8_wands
+    Nine of Wands = 9_wands
+    Ten of Wands = 10_wands
+    Page of Wands = page_wands
+    Knight of Wands = knight_wands
+    Queen of Wands = queen_wands
+    King of Wands = king_wands
+"""
 
-# print(introduction)
-print(instructions_2)
+cups_to_symbols = """
+    The Cups:
+    
+    Ace of Cups = 1_cups
+    Two of Cups = 2_cups
+    Three of Cups = 3_cups
+    Four of Cups = 4_cups
+    Five of Cups = 5_cups
+    Six of Cups = 6_cups
+    Seven of Cups = 7_cups
+    Eight of Cups = 8_cups
+    Nine of Cups = 9_cups
+    Ten of Cups = 10_cups
+    Page of Cups = page_cups
+    Knight of Cups = knight_cups
+    Queen of Cups = queen_cups
+    King of Cups = king_cups
+"""
+
+swords_to_symbols = """
+    The Swords:
+    
+    Ace of Swords = 1_swords
+    Two of Swords = 2_swords
+    Three of Swords = 3_swords
+    Four of Swords = 4_swords
+    Five of Swords = 5_swords
+    Six of Swords = 6_swords
+    Seven of Swords = 7_swords
+    Eight of Swords = 8_swords
+    Nine of Swords = 9_swords
+    Ten of Swords = 10_swords
+    Page of Swords = page_swords
+    Knight of Swords = knight_swords
+    Queen of Swords = queen_swords
+    King of Swords = king_swords
+"""
+
+pentacles_to_symbols = """
+    The Pentacles:
+    
+    Ace of Pentacles = 1_pentacles
+    Two of Pentacles = 2_pentacles
+    Three of Pentacles = 3_pentacles
+    Four of Pentacles = 4_pentacles
+    Five of Pentacles = 5_pentacles
+    Six of Pentacles = 6_pentacles
+    Seven of Pentacles = 7_pentacles
+    Eight of Pentacles = 8_pentacles
+    Nine of Pentacles = 9_pentacles
+    Ten of Pentacles = 10_pentacles
+    Page of Pentacles = page_pentacles
+    Knight of Pentacles = knight_pentacles
+    Queen of Pentacles = queen_pentacles
+    King of Pentacles = king_pentacles
+"""
