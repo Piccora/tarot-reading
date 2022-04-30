057f8df8af77b5564c6093ce0c49ae7f71609619
Piccora <tatuanminh31032004@gmail.com>
2022-04-30 16:46:30 +0700

    Added block text and further polishing code. Refactoring phase begins.

--- tarot.py
+++ tarot.py
@@ -4,7 +4,14 @@ import sys
 
 # Block text
 title_banner = """
-
+   /\     ,-----.    ,-----.       ,-----.     ,-----.   ,-----.    /\   
+  /  \       |      /       \     |       |   /       \     |      /  \  
+ /    \      |     |         |    |       |  |         |    |     /    \ 
+/      \     |     |_________|    |------/   |         |    |    /      \              
+\      /     |     |         |    |      \   |         |    |    \      /
+ \    /      |     |         |    |       |  |         |    |     \    / 
+  \  /       |     |         |    |       |   \       /     |      \  /  
+   \/        |     |         |    |       |    '-----'      |       \/   
 """
 
 def line():
@@ -66,22 +73,24 @@ class GameManager:
         else: print("Your input was incorrect, please try again.")
         line()
 
+    # def reset_game(self):
+    #     """Clears the necessary vars after finishing a game."""
+    #     Question.answered = 0
+    #     Question.answered_correct = 0
+
 class GameLoop:
     """Class for handling the flow of the game, such as asking for inputs"""
     
-    def answer(self, answer, result):
+    def answer(answer, result = 0):
         card = answer + "_" + str(result)
         if result == 0:
             card_0 = cards_positions_to_meaning[card]
-            self.answer_0 = answer
             return card_0
         elif result == 1:
             card_1 = cards_positions_to_meaning[card]
-            self.answer_1 = answer
             return card_1
         elif result == 2:
             card_2 = cards_positions_to_meaning[card]
-            self.answer_2 = answer
             return card_2
         else: return ValueError
     
@@ -89,21 +98,21 @@ class GameLoop:
         print("The result from your tarot reading was:")
         line()
         print("Yourself:")
-        print(list[0])
+        print(textwrap.fill(list[0], 70))
         line()
         print("Your partner:")
-        print(list[1])
+        print(textwrap.fill(list[1], 70))
         line()
         print("The result:")
-        print(list[2])
+        print(textwrap.fill(list[2], 70))
 
-    def card_error(self):
+    def card_error(check):
         """Checking whether two cards are the same or not"""
-        if self.answer_0 == self.answer_1:
+        if check[0] == check[1]:
             return ValueError
-        elif self.answer_1 == self.answer_2:
+        elif check[1] == check[2]:
             return ValueError
-        elif self.answer_0 == self.answer_2:
+        elif check[0] == check[2]:
             return ValueError
 menu = GameManager()
 
@@ -114,21 +123,51 @@ while True:
     
     if option == 1:
         print(textwrap.fill(instructions_2, 70))
-        print(instructions_3)
+        print(textwrap.fill(instructions_3, 70))
         i = 0
         card_answer = []
+        card_answer_position_regard = []
         while i < 3:
-            answer = input('Write your first card with the special syntax. For more information type "help"\n')
-            if answer in ["help", "help_major", "help_wands", "help_cups", "help_swords", "help_pentacles"]:
-                menu.display_help(answer)
-            elif answer in possible_answers:
-                card_answer.append(GameLoop.answer(answer, i))
-                i += 1
-            else: print("Your input was incorrect, please try again.")
-        validation = GameLoop.card_error() 
+            if i == 0:
+                answer = input('Write your first card with the special syntax. For more information type "help"\n')
+                if answer in ["help", "help_major", "help_wands", "help_cups", "help_swords", "help_pentacles"]:
+                    menu.display_help(answer)
+                elif answer in possible_answers:
+                    card_answer_position_regard.append(answer)
+                    card_answer.append(GameLoop.answer(answer, i))
+                    i += 1
+                    line()
+                else: print("Your input was incorrect, please try again.")
+            if i == 1:
+                answer = input('Write your second card with the special syntax. For more information type "help"\n')
+                if answer in ["help", "help_major", "help_wands", "help_cups", "help_swords", "help_pentacles"]:
+                    menu.display_help(answer)
+                elif answer in possible_answers:
+                    card_answer_position_regard.append(answer)
+                    card_answer.append(GameLoop.answer(answer, i))
+                    i += 1
+                    line()
+                else: print("Your input was incorrect, please try again.")
+            if i == 2:
+                answer = input('Write your third card with the special syntax. For more information type "help"\n')
+                if answer in ["help", "help_major", "help_wands", "help_cups", "help_swords", "help_pentacles"]:
+                    menu.display_help(answer)
+                elif answer in possible_answers:
+                    card_answer_position_regard.append(answer)
+                    card_answer.append(GameLoop.answer(answer, i))
+                    i += 1
+                    line()
+                else: print("Your input was incorrect, please try again.")
+        validation = GameLoop.card_error(card_answer_position_regard) 
         if validation == ValueError:
-            sys.exit("Two identical cards detected. Please input your cards all over again. The system will now restart.", 70)
-        GameLoop.result(card_answer)
+            # sys.exit(textwrap.fill("Two identical cards detected. Please input your cards all over again. The system will now restart.", 70))
+            print(textwrap.fill("Two identical cards detected. Please input your cards all over again. The system will now restart.", 70))
+            continue
+        else: 
+            GameLoop.result(card_answer)
+            restart = input('Type "restart" to restart the program\n')
+            if restart == "restart":
+                continue
                 
     elif option == 2:
         print("This is still a work in progress, please choose another question.")
